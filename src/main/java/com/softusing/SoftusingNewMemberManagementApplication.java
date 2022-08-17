ed239876af4a924aa9d8db4a3ed474291c5afaa7
Benno <benno@mygit.com>
2022-08-18 04:05:44 +0900

    首次备份，前端页面完成。

---
+++ src/main/java/com/softusing/SoftusingNewMemberManagementApplication.java
@@ -0,0 +1,13 @@
+package com.softusing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftusingNewMemberManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoftusingNewMemberManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/softusing/SoftusingNewMemberManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softusing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftusingNewMemberManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
